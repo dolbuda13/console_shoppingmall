b52bbd67d601ffe3836f51fd86c035d30710f46c
dolbuda13 <dolbuda24@gmail.com>
2024-10-31 17:33:27 +0900

    feat: 2번 기능 구현

--- bin/console_shoppingmall.dart
+++ bin/console_shoppingmall.dart
@@ -12,33 +12,77 @@ class Product {
 class ShoppingMall{
   List <Product> products = [];
   int totalprice = 0;
-//git test
+
   ShoppingMall(this.products); //생성인자 
 
   void showProducts(){
     for (var product in products){
-      print('${product.productname} / ${product.price}');
+      print('${product.productname} / ${product.price} 원' );
     }
   }
+
+  void addToCart(String productname, int quantity){
+    var product = products.firstWhere((p) => p.productname == productname);
+    totalprice += product.price * quantity;
+  }
+
 }
 
 void main() {
+  Product shirt = Product('shirt', 45000);
+  Product dress = Product('dress', 30000);
+  Product tshirt = Product('tshirt', 35000);
+  Product shorts = Product('shorts', 38000);
+  Product socks = Product('socks', 5000);
+
+  ShoppingMall showmall = ShoppingMall([shirt, dress, tshirt, shorts, socks]);//상품 목록 생성
+
+  List<String> productNames = [];//2번 용 상품 검색 리스트 만들기
+for (var product in showmall.products) {
+  productNames.add(product.productname);
+}
+
   bool onoff = true; //4를 입력하기 전까지 while문이 돌아가게 참으로 고정
   while (onoff) {
   print('[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료');
   var menu = int.tryParse(stdin.readLineSync() ?? '0');
-  if (menu == 1){
-    Product shirt = Product('셔츠', 45000);
-    Product dress = Product('원피스', 30000);
-    Product tshirt = Product('반팔티', 35000);
-    Product shorts = Product('반바지', 38000);
-    Product socks = Product('양말', 5000);
-
-    ShoppingMall showmall = ShoppingMall([shirt, dress, tshirt, shorts, socks]);
-
+  if (menu == 1){//상품 목록 보기
     showmall.showProducts();//출력메소드 이용
-  }else if (menu == 2){
 
+  }else if (menu == 2){//장바구니 담기
+    print('상품 이름을 입력해 주세요 !');
+    String ? wishproduct = stdin.readLineSync();
+
+    print('상품 개수를 입력해 주세요 !');
+    String? quantity = stdin.readLineSync();
+
+    if(!productNames.contains(wishproduct)){
+      print('입력값이 올바르지 않아요!');
+    }
+    else if(quantity==null){
+      print('입력값이 올바르지 않아요!');
+    }
+
+    else {
+      try {
+        int? countablequantity;
+        countablequantity = int.parse(quantity);//입력값이 정수인지 확인
+
+        if (countablequantity>0 && wishproduct!=null){//상품개수 양수인지 확인
+
+          showmall.addToCart(wishproduct, countablequantity);
+          print('장바구니에 상품이 담겼어요 !');
+        }
+        else{
+          print('0개보다 많은 개수의 상품만 담을 수 있어요 !');
+        }
+      }
+      catch (e){
+        print('입력값이 올바르지 않아요!');
+      }
+    }
+
+    
   }else if (menu == 3){
 
   }else if (menu == 4){
